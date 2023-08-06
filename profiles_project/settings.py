d6132affc49464f05b253b34258b2bccc57d50a8
Chase <chase.lindsay@codelation.com>
2023-08-06 16:40:16 -0600

    added custom user profile

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile' # This is the model we created in profiles_api/models.py
