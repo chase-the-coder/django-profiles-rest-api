62ba337cf6337d1c8473c196eb7871c6b03fb1ee
Chase <chase.lindsay@codelation.com>
2023-08-07 17:24:13 -0600

    created view set methods

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
 from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name="hello-viewset") # base_name is required for ViewSets
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()), # as_view() is required for APIView
+    path('', include(router.urls)),
 ]
