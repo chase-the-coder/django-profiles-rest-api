abe091996f98a899226587ad590cae3a9bb21b9c
Chase <chase.lindsay@codelation.com>
2023-08-06 20:53:29 -0600

    added serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
@@ -7,6 +10,7 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
+        serializer_class = serializers.HelloSerializer
 
         an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
@@ -17,3 +21,12 @@ class HelloApiView(APIView):
 
         # Response must be a dictionary or a list
         return Response({'message': 'Hello!', 'an_apiview': an_apiview}) # Response is required for APIView
+    
+    def post(self, request):
+        """Create a hello message with our name."""
+        serializer = serializers.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!' # f is required for string interpolation
+            return Response({'message': message})
